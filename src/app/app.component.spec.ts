5e5178ec4b199833abdbaf2f39cb11e07da91df7
Jesse D'Angelo <dangelo.jesse@gmail.com>
2021-03-31 19:34:18 -0500

    Fix Tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { FormlyModule } from '@ngx-formly/core';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        FormlyModule,
+        ReactiveFormsModule,
       ],
       declarations: [
         AppComponent
